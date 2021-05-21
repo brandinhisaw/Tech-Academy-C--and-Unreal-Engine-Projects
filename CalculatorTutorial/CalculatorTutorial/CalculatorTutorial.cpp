a8c7502b695588a5b7436f0c6c99a04efc3ab3c2
Brandin Hisaw <brandin.hisaw@gmail.com>
2021-05-21 11:25:47 -0700

    Add Calculator Project to Repo

---
+++ CalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
@@ -0,0 +1,14 @@
+// CalculatorTutorial.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+        << endl;
+    return 0;
+}
